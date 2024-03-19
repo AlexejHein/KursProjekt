c843c26009dd3c9d5007ee72ee08bd14a1270922
Alexej Hein <alexej.hein@aol.de>
2024-03-19 09:00:09 +0100

    commit

--- KursProjekt/urls.py
+++ KursProjekt/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import veiws
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', veiws.index, name='index')
+    path('', include('polls.urls'))
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Poll, Choice
+
+
+def index(request):
+    antwort = ""
+    for poll in Poll.objects.all():
+        antwort += poll.name + "<br>"
+        for choice in poll.choice_set.all():
+            antwort += choice.name + "<br>"
+    return HttpResponse(antwort)
