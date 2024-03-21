6833e025b9276b37f150151fd630d776373245fd
Alexej Hein <alexej.hein@aol.de>
2024-03-21 16:57:28 +0100

    commit

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 from .models import Poll, Choice
 
 
 def index(request):
-    antwort = ""
-    for poll in Poll.objects.all():
-        antwort += poll.name + "<br>"
-        for choice in poll.choice_set.all():
-            antwort += choice.name + "<br>"
-    return HttpResponse(antwort)
+    context = {
+        'umfragen': Poll.objects.all(),
+        'title': "Umfragen",
+    }
+    return render(request, 'polls/index.html', context)
