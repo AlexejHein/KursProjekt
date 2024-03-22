79a2bd96b97b0a55e7bc94cf595b5449b84931ec
Alexej Hein <alexej.hein@aol.de>
2024-03-22 07:40:10 +0100

    commit

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index, umfrage_deteil
+from .views import index, umfrage_deteil, vote, results
 
 urlpatterns = [
     path('', index, name='index'),
     path('abstimmung/<str:slug>/', umfrage_deteil, name='umfrage_deteil'),
+    path('abstimmung/<str:slug>/abstimmen/', vote, name='vote'),
+    path('abstimmung/<str:slug>/results/', results, name='results'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -16,3 +16,21 @@ def umfrage_deteil(request, slug):
     umfrage = get_object_or_404(Poll, slug=slug)
     context = {'umfrage': umfrage}
     return render(request, 'polls/umfrage.html', context)
+
+
+def vote(request, slug):
+    umfrage = get_object_or_404(Poll, slug=slug)
+    try:
+        ausgewahlte_antwort = umfrage.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return HttpResponse("Fehler: Keine Antwort ausgewählt.")
+    else:
+        ausgewahlte_antwort.votes += 1
+        ausgewahlte_antwort.save()
+        return HttpResponse("Danke fürs Abstimmen!")
+
+
+def results(request, slug):
+    umfrage = get_object_or_404(Poll, slug=slug)
+    context = {'umfrage': umfrage}
+    return render(request, 'polls/results.html', context)
