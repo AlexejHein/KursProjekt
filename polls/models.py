a9ffbd70f5b16e6ace402f784afba340220a87b9
Alexej Hein <alexej.hein@aol.de>
2024-03-22 07:06:06 +0100

    commit

--- polls/models.py
+++ polls/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Poll(models.Model):
     name = models.CharField(max_length=256)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.name + ' (' + self.slug + ')'
