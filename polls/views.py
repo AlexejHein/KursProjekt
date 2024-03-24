11f448fe0b42933062b1f956468c453997f6bbbf
Alexej Hein <alexej.hein@aol.de>
2024-03-24 08:19:34 +0100

    commit

--- polls/views.py
+++ polls/views.py
@@ -23,6 +23,16 @@ class ResultsDeteilView(generic.DetailView):
     model = Poll
     template_name = 'polls/results.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_can_see_results = False
+        if 'voted_polls' in self.request.session:
+            print("In ResultsDeteilView: ", self.request.session['voted_polls'])  # Debugging-Ausgabe
+            if str(self.object.id) in self.request.session['voted_polls']:  # Überprüfung anpassen
+                user_can_see_results = True
+        context['access'] = user_can_see_results
+        return context
+
 
 def vote(request, slug):
     umfrage = get_object_or_404(Poll, slug=slug)
@@ -33,4 +43,13 @@ def vote(request, slug):
     else:
         ausgewahlte_antwort.votes += 1
         ausgewahlte_antwort.save()
+        if 'voted_polls' in request.session:
+            print("Vor dem Hinzufügen: ", request.session['voted_polls'])  # Debugging-Ausgabe
+            if type(request.session['voted_polls']) != list:
+                request.session['voted_polls'].append(str(umfrage.id))  # umfrage.id in einen String umwandeln
+            else:
+                request.session['voted_polls'] = [str(umfrage.id)]  # umfrage.id in einen String umwandeln
+            print("Nach dem Hinzufügen: ", request.session['voted_polls'])  # Debugging-Ausgabe
+        else:
+            request.session['voted_polls'] = [str(umfrage.id)]  # umfrage.id in einen String umwandeln
         return HttpResponseRedirect(reverse('polls:results', args=(umfrage.slug,)))
